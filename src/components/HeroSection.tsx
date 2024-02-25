3e10701cfd21082e949ea93f3f76b46eb9075d6c
Suvra Ghosh <ghoshsuvra2566@gmail.com>
2024-02-25 22:55:27 +0530

    Added Testimonial Cards

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,7 +5,7 @@ import { Button } from "./ui/moving-border";
 
 function HeroSection() {
   return (
-    <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
+    <div className="h-auto dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.1] md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
       <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="red"
